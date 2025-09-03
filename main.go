75d24da479510e88c72ee428f79b1a13e184b735
mvusic07 <thinkman405@gmail.com>
2025-09-03 15:03:12 +0200

    end of ch4

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"sync/atomic"
 )
@@ -18,9 +20,14 @@ func (apiCfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (apiCfg *apiConfig) handlerCount(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	hits := apiCfg.fileServerHits.Load()
-	response := fmt.Sprintf("Hits: %d", hits)
+	response := fmt.Sprintf(`<html>
+	<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+	</body>
+	</html>`, hits)
 	w.Write([]byte(response))
 }
 
@@ -29,6 +36,69 @@ func (apiCfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hits reset to 0"))
+}
+
+func validateHandler(w http.ResponseWriter, r *http.Request) {
+
+	type parametri struct {
+		Tijelo string `json:"body"`
+	}
+	type greska struct {
+		Error string `json:"error"`
+	}
+	greskainst1 := greska{
+		Error: "Something went wrong",
+	}
+	greskainst2 := greska{
+		Error: "Chirp is too long",
+	}
+
+	datt, err := json.Marshal(greskainst1)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	dataa, errr := json.Marshal(greskainst2)
+	if errr != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parametri{}
+	er := decoder.Decode(&params)
+	if er != nil {
+		w.WriteHeader(500)
+		w.Write(datt)
+		return
+	}
+	if len(params.Tijelo) > 140 {
+		w.WriteHeader(400)
+		w.Write(dataa)
+		return
+
+	}
+	type odgovor struct {
+		Valid bool `json:"valid"`
+	}
+	odgovorb := odgovor{
+		Valid: true,
+	}
+	dat, err := json.Marshal(odgovorb)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write(datt)
+		return
+
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(dat)
+
 }
 
 func main() {
@@ -40,13 +110,15 @@ func main() {
 	}
 	handler := http.FileServer(http.Dir("."))
 	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(handler)))
-	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type:", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
-	mux.HandleFunc("GET /metrics", apiCfg.handlerCount)
-	mux.HandleFunc("/reset", apiCfg.resetHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerCount)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateHandler)
+
 	server.ListenAndServe()
 
 }
