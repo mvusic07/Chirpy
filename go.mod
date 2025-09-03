fbfb94d6ebc02a974da8c190bccb6ff63c4234e0
mvusic07 <thinkman405@gmail.com>
2025-09-03 12:53:45 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mvusic07/Chirpy
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (apiCfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (apiCfg *apiConfig) handlerCount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	hits := apiCfg.fileServerHits.Load()
+	response := fmt.Sprintf("Hits: %d", hits)
+	w.Write([]byte(response))
+}
+
+func (apiCfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	apiCfg.fileServerHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
+
+func main() {
+	apiCfg := &apiConfig{}
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":08080",
+		Handler: mux,
+	}
+	handler := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(handler)))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type:", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	mux.HandleFunc("/metrics", apiCfg.handlerCount)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
+	server.ListenAndServe()
+
+}
